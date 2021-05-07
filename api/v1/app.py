7768ccab772daf1acc09f295825a71e8c32a86c9
martin saavedra <martin@bildung.uy>
2021-05-07 20:22:13 +0000

    task 7

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,8 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
 
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,18 +1,53 @@
 #!/urs/bin/python3
 '''Flask module'''
 
-
-from flask import jsonify
+import collections
+from flask import jsonify, request
 from models import storage
 from api.v1.views import app_views
+from api.v1 import app 
 from models.state import State
 
-@app_views.route('/states', methods=['GET'])
-def list_states():
+
+@app_views.route('/states/', methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET'])
+def list_states(state_id=None):
     '''list states'''
     states = storage.all(State)
-    states = list(obj.to_json() for obj in states.values())
-    return jsonify(states)
+    states = list(obj.to_dict() for obj in states.values())
+    if not state_id:
+        return jsonify(states)
+    elif state_id != None:
+        for state in states:
+            if state['id'] == state_id:
+                return jsonify(state)
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id=None):
+    '''Deletes an states by ID'''
+    states = storage.all(State)
+    states = list(obj.to_dict() for obj in states.values())
+    if state_id:
+        for index, state in enumerate(states):
+            if state['id'] == state_id:
+                state = storage.get(State, state_id)
+                storage.delete(state)
+                return jsonify({}), 200
+
+@app_views.route('/states/', methods=['POST'])
+def post_state():
+    '''Post changes in Object state'''
+    state_json = request.get_json()
+    if not state_json:
+        abort(400, 'Not a Json')
+    elif state_json.get("name") is None:
+        abort(400, 'Missing name')
+    else:
+        state_obj = State(**state_json)
+        state_obj.save()
+        state_obj = state_obj.to_dict()
+        return jsonify(state_obj), 201
+
 
 
 
