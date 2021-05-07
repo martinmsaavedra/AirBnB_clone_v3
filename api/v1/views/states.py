cb5b801bffeee8a48505c1007cd569e0ece5b451
martin saavedra <martin@bildung.uy>
2021-05-07 22:46:07 +0000

    task 7

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,8 +9,8 @@ from api.v1 import app
 from models.state import State
 
 
-@app_views.route('/states/', methods=['GET'])
-@app_views.route('/states/<state_id>', methods=['GET'])
+@app_views.route('/states/', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def list_states(state_id=None):
     '''list states'''
     states = storage.all(State)
@@ -21,8 +21,9 @@ def list_states(state_id=None):
         for state in states:
             if state['id'] == state_id:
                 return jsonify(state)
+    abort(404)
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id=None):
     '''Deletes an states by ID'''
     states = storage.all(State)
@@ -33,8 +34,9 @@ def delete_state(state_id=None):
                 state = storage.get(State, state_id)
                 storage.delete(state)
                 return jsonify({}), 200
+    abort(404)
 
-@app_views.route('/states/', methods=['POST'])
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
 def post_state():
     '''Post changes in Object state'''
     state_json = request.get_json()
@@ -47,6 +49,7 @@ def post_state():
         state_obj.save()
         state_obj = state_obj.to_dict()
         return jsonify(state_obj), 201
+    abort(404)
 
 @app_views.route('states/<state_id>', methods=['PUT'])
 def put_state(state_id=None):
@@ -64,3 +67,4 @@ def put_state(state_id=None):
                     state.save()
                     state = state.to_dict()
                     return jsonify(state), 200
+    abort(404)
