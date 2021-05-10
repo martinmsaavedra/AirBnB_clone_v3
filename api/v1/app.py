4d00245c6ab24552b00741bf950bb4151127c53a
Matias Zappino <matiaszappino31@gmail.com>
2021-05-10 13:57:42 +0000

    Ex 8 Fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Flask Model"""
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.register_blueprint(app_views)
 
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
